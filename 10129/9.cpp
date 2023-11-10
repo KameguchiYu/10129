144351601b416e55de7163271bdcdd378291bc18
KameguchiYu <yu.kameguchi8010@gmail.com>
2023-11-10 11:32:55 +0900

    プロジェクト ファイルを追加します。

---
+++ 10129/9.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void AverageScore(int* point, int size)
+{
+	float average = 0;
+	float sum = 0;
+	for (int i = 0; i < size; ++i)
+	{
+		cout << i + 1 << "人目を入力してください。" << flush;
+		cin >> point[i];
+	}
+	for (int j = 0; j < size; ++j)
+	{
+		sum += point[j];
+		average = sum / size;
+	}
+	cout << "平均点は" << average << "です。" << endl;
+}
+
+int main()
+{
+	int size;
+	int* point;
+	cout << "何名入力しますか？" << flush;
+	cin >> size;
+
+	point = new int[size];
+	AverageScore(point, size);
+
+	delete[] point;
+}
